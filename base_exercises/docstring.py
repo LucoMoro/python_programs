dacb82a86cd36fba2890d19285f4ddac0096a243
LucoMoro <morelli.2000luca@gmail.com>
2022-12-07 20:24:00 +0100

    Simulating project with yelp

---
+++ base_exercises/docstring.py
@@ -0,0 +1,15 @@
+class Converter:
+
+    """ A simple converter to convert a PDF to text"""
+    def convert(self, path):
+        """ 
+        Convert the given PDF to text.
+        
+        Parameters:
+        path (str): The path to a PDF file
+
+        Returns:
+        str: The content of the PDF files as text.
+        """
+        print("pdf2text")
+
